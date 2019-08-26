99f84506f840bbfbd4924bab5f319137d954ae19
CaptainJi <jiqing19861123@163.com>
2019-08-26 16:47:19 +0800

    更新报告生成部分

---
+++ unittest/Test_Baidu/test_case/test_baidu.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+import unittest
+from time import sleep
+
+class TestBaidu(unittest.TestCase):
+    def setUp(self):
+        self.driver=webdriver.Chrome()
+        self.driver.implicitly_wait(10)
+        self.driver.get('http://www.baidu.com')
+
+    def test_baidu(self):
+        driver=self.driver
+        driver.find_element_by_id('kw').clear()
+        driver.find_element_by_id('kw').send_keys('Selenium')
+        driver.find_element_by_id('su').click()
+        sleep(3)
+
+        title=driver.title
+        self.assertEqual(title,'Selenium_百度搜索')
+
+        driver.find_element_by_link_text('Selenium(浏览器自动化测试框架)_百度百科').click()
+        sleep(5)
+
+    def tearDown(self) -> None:
+        self.driver.quit()
+
+if __name__ == '__main__':
+    unittest.main()
